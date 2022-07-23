e355423744325264978298f8e908703ecb7967af
Dane <Dseatondesign@gmail.com>
2022-07-23 18:49:22 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// for loop
+// for ([initialization]; [condition]; [iteration]) {
+//   [loop body]
+// }
+
+function writeCards(names, event) {
+  const results = [];
+  for (let i = 0; i < names.length; i++) {
+    results.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return results
+}
+
+function countDown(number){
+  for (let i = 0; i <= number; i++) {
+    console.log(i)
+      }
+}
